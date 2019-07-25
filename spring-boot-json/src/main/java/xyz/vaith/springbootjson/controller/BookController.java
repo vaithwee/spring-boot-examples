6d5ad45d4d235e87daebfff013f3d5c283dd2619
Vaith <vaithwee@yeah.net>
2019-07-25 09:08:29 +0800

    Add list to json example

--- spring-boot-json/src/main/java/xyz/vaith/springbootjson/controller/BookController.java
+++ spring-boot-json/src/main/java/xyz/vaith/springbootjson/controller/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.vaith.springbootjson.domain.Book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @Log4j2
 public class BookController {
@@ -19,5 +22,14 @@ public class BookController {
         book.setId(199);
         log.info(book.toString());
         return book;
+    }
+
+    @RequestMapping("/findBooks")
+    public List<Book> findBooks() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1, "name1", "author1", "image1", 19.9, "remark1"));
+        books.add(new Book(2, "name2", "author2", "image2", 29.9, "remark2"));
+        books.add(new Book(3, "name3", "author3", "image3", 39.9, "remark3"));
+        return books;
     }
 }
--- spring-boot-json/src/main/java/xyz/vaith/springbootjson/controller/IndexController.java
+++ spring-boot-json/src/main/java/xyz/vaith/springbootjson/controller/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/getjson")
+    public String getjson() {
+        return "getjson";
+    }
 }

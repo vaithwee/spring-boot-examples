e6fe58c54ad72b7f549e68d81c899a4a9cb2e8bc
Vaith <vaithwee@yeah.net>
2019-08-12 11:23:22 +0800

    Add hikari data source demo

---
+++ spring-boot-hikari-cp/src/main/java/xyz/vaith/springboothikaricp/SpringBootHikariCpApplication.java
@@ -0,0 +1,13 @@
+package xyz.vaith.springboothikaricp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHikariCpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHikariCpApplication.class, args);
+    }
+
+}
